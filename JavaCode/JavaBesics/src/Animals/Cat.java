d9861d99d4025cb8e7a1d93e6676572a2f4aae9e
unknown <shreyatasugandhi>
2018-09-24 23:09:36 -0400

    Adding JavaCode

---
+++ JavaCode/JavaBesics/src/Animals/Cat.java
@@ -0,0 +1,7 @@
+package Animals;
+
+
+// Inheritance - Class Cat is a subclass/chieldclass of Animal 
+public class Cat extends Animal{
+
+}
---
+++ JavaCode/JavaBesics/src/Animals/Dog.java
@@ -0,0 +1,8 @@
+package Animals;
+
+
+//Inheritance - Class Dog is a subclass/chieldclass of Animal
+
+public class Dog extends Animal{
+
+}
